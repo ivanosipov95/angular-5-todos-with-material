66872eb5df3fecaaf500b8bc82c549f56e575141
Ivan Osipov <ivan_osipov@epam.com>
2018-03-19 00:41:12 +0300

    added vertical scroll

--- src/app/tasks/services/active-tasks-resolve.service.ts
+++ src/app/tasks/services/active-tasks-resolve.service.ts
@@ -2,10 +2,8 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
-import {
-  TasksService,
-  Task
-} from '../';
+import {TasksService} from '../';
+import {Task} from '../model';
 
 @Injectable()
 export class ActiveTasksResolveService implements Resolve<Task[]> {
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -9,6 +9,21 @@ import {Task} from '../model/task.model';
 export class TaskComponent implements OnInit {
   @Input() task: Task;
 
+  taskOptions = [
+    {
+      icon: 'mode_edit',
+      label: 'Edit'
+    },
+    {
+      icon: 'remove',
+      label: 'Remove'
+    },
+    {
+      icon: 'done',
+      label: 'Complete'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
